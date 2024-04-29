e910b640b742a9c71b9c3b734f939c11ed4fe18f
adistanautiyal <adistanautiyal@gmail.com>
2024-04-29 17:46:41 +0530

    saving the changes in adista's computer to github

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import SideBar from "./components/sidebar/sidebar.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  
   useEffect(() => {
     let timer = setTimeout(() => {
     setCount((count) => count + 1);
   }, 1000);
   }, []);
 
-  return <h1>I've rendered {count} times!</h1>;
+  return (
+  <>
+  <SideBar/>
+  <h1>I've rendered {count} times!</h1>
+  </>
+)
 }
 
 export default App;
