6f5744ee5484858c316966e0625ffb2ea23637e3
adistanautiyal <adistanautiyal@gmail.com>
2024-04-30 12:49:45 +0530

    fixing routing issue

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -5,7 +5,7 @@ function SideBar() {
   return (
     <>
         <aside className="h-full w-16 flex flex-col space-y-10 items-center justify-center relative bg-gray-800 text-white">
-          <NavLink className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
+          <NavLink to="/a" className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
             
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -21,7 +21,7 @@ function SideBar() {
             </svg>
           </NavLink>
 
-          <div className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
+          <NavLink to="/b" className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
@@ -36,9 +36,9 @@ function SideBar() {
                 d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
               />
             </svg>
-          </div>
+          </NavLink>
 
-          <div className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
+          <NavLink to="/c" className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
@@ -53,7 +53,7 @@ function SideBar() {
                 d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
               />
             </svg>
-          </div>    
+          </NavLink>    
         </aside>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -7,24 +7,26 @@ import Content1 from "./components/contents/content1.jsx";
 import Content2 from "./components/contents/content2.jsx";
 import Content3 from "./components/contents/content3.jsx";
 
-const router = createBrowserRouter({
-  path: "/",
-  element: <App />,
-  children: [
-    {
-      path: "a",
-      element: <Content1 />
-    },
-    {
-      path: "b",
-      element: <Content2 />
-    },
-    {
-      path: "c",
-      element: <Content3 />
-    }
-  ]
-});
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "a",
+        element: <Content1 />,
+      },
+      {
+        path: "b",
+        element: <Content2 />,
+      },
+      {
+        path: "c",
+        element: <Content3 />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
